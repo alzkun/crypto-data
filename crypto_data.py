a74be47093fa157adea123fe8de3014e0e7837fa
Alex <alz.root@gmail.com>
2017-08-24 01:22:22 +0200

    Add current value
    Add value data for all crypto currencies owned by the user.
    Values are in BTC and EUR.

--- crypto_data.py
+++ crypto_data.py
@@ -6,6 +6,25 @@ from poloniex_interface import PoloniexInterface
 
 from prettytable import PrettyTable
 
+def amount(argv, data):
+    ratio_btc = float(data['BTC_EUR'][0])
+
+    coins = {
+            'BTC': [argv.btc, float(1.0)],
+            'LTC': [argv.ltc, float(data['LTC_BTC'][0])],
+            'BCH': [argv.bch, float(data['BCH_BTC'][0])],
+            'ETH': [argv.eth, float(data['ETH_BTC'][0])],
+            'ETC': [argv.etc, float(data['ETC_BTC'][0])]
+    }
+
+    ret = {}
+
+    for k, v in coins.items():
+        if (v[0] != None):
+            ret[k] = [v[0], v[0]*v[1], v[0]*v[1]*ratio_btc]
+
+    return ret
+
 def main(argv):
     if (argv.exchange == 'polo'):
         exchange = PoloniexInterface()
@@ -19,6 +38,21 @@ def main(argv):
         pairs_table.add_row([k, v[0], v[1], v[2]])
 
     print(pairs_table)
+
+    current_coins = amount(argv, ticker_data)
+
+    summary = [0.0, 0.0]
+
+    if (argv.exchange == 'kraken' and current_coins):
+        coins_table = PrettyTable(['Coin', 'QTY', 'BTC', 'EUR'])
+        for k, v in current_coins.items():
+            coins_table.add_row([k, round(v[0], 8), round(v[1], 8), round(v[2], 2)])
+            summary = [summary[0]+v[1], summary[1]+v[2]]
+
+        coins_table.add_row(['', '', '', ''])
+        coins_table.add_row(['', '', round(summary[0], 8), round(summary[1], 2)])
+        print(coins_table)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Show ticker data for major crypto currencies.')
